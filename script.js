60e205c1492ca5ea0dcf9be338db9fae89317fda
Carmen Obied Lopez <you@example.com>
2020-05-01 19:22:50 -0700

    fixed color change function and now planner is working effectively, using each() method and parseInt and if else statements for color changing based on time. made changes to rows and syntax in html

--- script.js
+++ script.js
@@ -7,53 +7,50 @@
     console.log(currentDay)
     $("#currentDay").text(currentDay);
 
-    // Set military time
+    // Set current time to military time
     var presentTime = moment().format("H");
     console.log(presentTime);
 
-    // JSON.parse(localStorage.getItem()
-
     // call colour change funtion
-    timeblockColour();
+    timeblockColor();
 
-    // change timeblock colour based on past, presebt, future
-    function timeblockColour() {
-        if ($(".timeblockRow").attr("value") === presentTime) {
-            // timeblock colour for past = grey
-            $(".eventDescription").addClass("present");
-            } else if ($(".timeblockRow").attr("value") > presentTime) {
-                // timeblock colour for past = green
-                $(".eventDescription").addClass("past");
-            } else if ($(".timeblockRow").attr("value") < presentTime) {
-            // timeblock colour for past = red
-            $(".eventDescription").addClass("future");
+    // Following function will separate values of each block and change color according to time
+    function timeblockColor() {
+        $(".timeblockRow").each(function() {
+            var eventInput = parseInt($(this).attr("value"));
+            // Compare each timeblock to current hour and change color
+            if (eventInput < presentTime) {
+                $(this).addClass("past");
+            }else if (eventInput == presentTime) {
+                $(this).addClass("present");
+            }else if (eventInput > presentTime) {
+                $(this).addClass("future");
             }
-            console.log($(".timeblockRow").attr("value"));
-       }
-
+        })
+    }
+    
         // Save Button event listenr
        $(".saveBtn").on("click", function () {
         //setting items in the local storage
-        localStorage.setItem("9AM", ($("#event1").val()))
-        localStorage.setItem("10AM", ($("#event2").val()))
-        localStorage.setItem("11AM", ($("#event3").val()))
-        localStorage.setItem("12AM", ($("#event4").val()))
-        localStorage.setItem("13PM", ($("#event5").val()))
-        localStorage.setItem("14PM", ($("#event6").val()))
-        localStorage.setItem("15PM", ($("#event7").val()))
-        localStorage.setItem("16PM", ($("#event8").val()))
-        localStorage.setItem("17PM", ($("#event9").val()))
+        localStorage.setItem("9", ($("#event1").val()))
+        localStorage.setItem("10", ($("#event2").val()))
+        localStorage.setItem("11", ($("#event3").val()))
+        localStorage.setItem("12", ($("#event4").val()))
+        localStorage.setItem("13", ($("#event5").val()))
+        localStorage.setItem("14", ($("#event6").val()))
+        localStorage.setItem("15", ($("#event7").val()))
+        localStorage.setItem("16", ($("#event8").val()))
+        localStorage.setItem("17", ($("#event9").val()))
     })
 
         //get stored items in the local storage and append. When refreshed, stored items display in the planner
-        $("#event1").append(localStorage.getItem("9AM"));
-        $("#event2").append(localStorage.getItem("10AM"));
-        $("#event3").append(localStorage.getItem("11AM"));
-        $("#event4").append(localStorage.getItem("12AM"));
-        $("#event5").append(localStorage.getItem("13PM"));
-        $("#event6").append(localStorage.getItem("14PM"));
-        $("#event7").append(localStorage.getItem("15PM"));
-        $("#event8").append(localStorage.getItem("16PM"));
-        $("#event9").append(localStorage.getItem("17PM"));
-    })
-
+        $("#event1").append(localStorage.getItem("9"));
+        $("#event2").append(localStorage.getItem("10"));
+        $("#event3").append(localStorage.getItem("11"));
+        $("#event4").append(localStorage.getItem("12"));
+        $("#event5").append(localStorage.getItem("13"));
+        $("#event6").append(localStorage.getItem("14"));
+        $("#event7").append(localStorage.getItem("15"));
+        $("#event8").append(localStorage.getItem("16"));
+        $("#event9").append(localStorage.getItem("17"));
+})
